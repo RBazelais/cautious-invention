16cdee639dd2bb7623072a43deeaf42fe22377f3
rbazelais <bazelais.rachel@gmail.com>
2018-02-06 14:52:44 -0800

    survey form complete

--- survey_form/apps/main/views.py
+++ survey_form/apps/main/views.py
@@ -8,25 +8,39 @@ def index(request):
 
 def reset(request):
     request.session.flush()
-    return redirect('/')
+    return redirect('/result')
 
 def process(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        print request.POST
+        print request.POST['name']
+        print request.POST['location']
+        print request.POST['language']
+        print request.POST['comment']
+
+        request.session['name'] = request.POST['name']
+        request.session['location'] = request.POST['location']
+        request.session['language'] = request.POST['language']
+        request.session['comment'] = request.POST['comment']
+
+    # context = {
+    #     'count': count,
+    #     # 'name': name,
+    #     # 'location': location,
+    #     # 'langauge': language,
+    #     # 'comment': comment
+    # }
+    return redirect('/result')
+
+def result(request):
     count = 0
     if 'count' in request.session:
         request.session['count'] += 1
     else:
         request.session['count'] = 1
 
-    context = {
-        'count': count,
-        'name': request.POST['name'],
-        'location': request.POST['location'],
-        'langauge': request.POST['langauge']
-    }
-    return redirect('/result')
+    return render(request, 'display.html', { 'count' : count })
 
-def result(request):
-    return render(request, 'display.html', context)
-    
 def go_back(request):
     return redirect('/')
