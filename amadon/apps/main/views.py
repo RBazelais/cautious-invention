755993cf7e9463844d7343484aa096abe885a441
rbazelais <bazelais.rachel@gmail.com>
2018-02-12 14:14:36 -0800

    Amadon complete: Now displays all data based on buy button

--- amadon/apps/main/views.py
+++ amadon/apps/main/views.py
@@ -4,6 +4,11 @@ from django.utils.crypto import get_random_string
 
 # Create your views here.
 def index(request):
+    if not 'total' in request.session:
+        request.session['total'] = 0
+    if not 'price' in request.session:
+        request.session['price'] = 0
+    request.session['buy'] = []
     return render(request, 'index.html')
 
 def reset(request):
@@ -13,7 +18,7 @@ def reset(request):
 def process(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        shirt = request.POST.get('shirt', False)
+        shirt = request.POST.get('shirt', '')
         sweater = request.POST.get('sweater', False)
         mug = request.POST.get('mug', False)
         algo_book = request.POST.get('algo_book', False)
@@ -34,35 +39,69 @@ def process(request):
     return redirect('/checkout', context)
 
 def checkout(request):
-    productDict ={
-        'shirt' : 20,
-        'sweater': 30,
-        'mug': 5,
-        'algo_book': 50
-    }
-    count = 0
+    
+    buy = request.POST['buy']
+    total = request.session['total']
+    count = request.session['count']
+    price = request.session['price']
+
     if 'count' in request.session:
         request.session['count'] += 1
     else:
         request.session['count'] = 1
+    print "init total: ", total
 
-    total = 0
-    total = request.POST.get(['shirt'].val())
-    print total
+    def purchase():
+        pass
 
-    if 'total' in request.session:
-        if 'shirt' in request.session:
-            total = shirt.value 
-        elif 'sweater' in request.session:
-            request.session['total'] += 30
-        elif 'mug' in request.session:
-            request.session['total'] += 5
-        elif 'algo_book' in request.session:
-            request.session['total'] += 50
+    # if shirt clicked do a thing
+    if buy == 'shirt':
+        # does it work?
+        print "Buying a shirt..."
+        shirt = 19.99
+        request.session['price'] = shirt
+        #send total to session, then increment session total
+        request.session['total'] = total + shirt
+        print "New total: ", total
+        print "New count: ", count
+
+    elif buy == 'sweater':
+        # does it work?
+        print "Buying a sweater..."
+        sweater = 29.99
+        request.session['price'] = sweater
+        #send total to session, then increment session total
+        request.session['total'] = total + sweater 
+
+        print "New total: ", total
+        print "New count: ", count
+
+    elif buy == 'mug':
+        # does it work?
+        print "Buying a mug..."
+        mug = 4.99
+        request.session['price'] = mug
+        #send total to session, then increment session total
+        request.session['total'] = total + mug 
+
+        print "New total: ", total
+        print "New count: ", count
+
+    elif buy == 'algo_book':
+        # does it work?
+        print "Buying an algorithm book..."
+        algo_book = 29.99
+        request.session['price'] = algo_book
+        #send total to session, then increment session total
+        request.session['total'] = total + algo_book 
+
+        print "New total: ", total
+        print "New count: ", count
+
     else:
-        request.session['total'] = 0
-
-    return render(request, 'display.html', { 'count' : count, 'total': total })
+        request.session['total'] = 'null'
+   
+    return render(request, 'display.html', { 'count' : count, 'total': total, 'price': price })
 
 def go_back(request):
     return redirect('/')
